7a2f37eaf9e18bc56a33e790d596ce364ca8b91a
JuanLema14 <72539768+JuanLema14@users.noreply.github.com>
2023-11-05 17:13:20 -0500

    descarga de CV

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,21 +1,34 @@
 <template>
-  <q-page style="padding: 0.6vmax;">
+  <q-page style="padding: 0.6vmax">
     <q-card flat square>
       <q-img
         src="~assets/PS - Hoja de vida.png"
         style="width: 98%; height: 24vmax; position: absolute"
       >
-        <div class="absolute-bottom-left"><q-btn color="warning" label="contactame" href="mailto:juan.lema1408@gmail.com"/></div>
+        <div class="absolute-bottom-left">
+          <q-btn
+            color="warning"
+            label="contactame"
+            href="mailto:juan.lema1408@gmail.com"
+          />
+        </div>
+        <div class="absolute-bottom-right">
+          <span class="q-pr-sm">Descargar CV</span>
+          <q-btn round size="1vmax" flat @click="downloadCV">
+            <q-icon color="white" name="fas fa-file-pdf" />
+          </q-btn>
+        </div>
       </q-img>
-
     </q-card>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  name: "IndexPage",
-});
+<script setup>
+function downloadCV() {
+  const filePath = "src/assets/Doc/CV- Juan Andres Lema.pdf";
+  const link = document.createElement("a");
+  link.href = filePath;
+  link.download = "CV - Juan Andres Lema.pdf";
+  link.click();
+}
 </script>
